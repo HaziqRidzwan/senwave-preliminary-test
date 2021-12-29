308577c4ea4957f07c26960430b0b50888c75ab0
HaziqRidzwan <haziqridzwan@outlook.com>
2021-12-29 17:39:03 +0800

    Initial commit

---
+++ src/main/java/com/senwave/spt/SenwavePreliminaryTestApplication.java
@@ -0,0 +1,13 @@
+package com.senwave.spt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenwavePreliminaryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenwavePreliminaryTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senwave/spt/SenwavePreliminaryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senwave.spt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenwavePreliminaryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
